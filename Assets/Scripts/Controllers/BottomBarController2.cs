f7094aa2525465a6981aea06385505eee429731b
Raekwon Laptop <raekwongerold@hotmail.com>
2023-05-08 12:12:33 +0200

    Size Changes to textbox in chat based backgrounds

--- Assets/Scripts/Controllers/BottomBarController2.cs
+++ Assets/Scripts/Controllers/BottomBarController2.cs
@@ -163,7 +163,7 @@ public class BottomBarController2 : MonoBehaviour
         while (state != State.COMPLETED)
         {
             barText.text += text[wordIndex];
-            yield return new WaitForSeconds(speedFactor * 0.05f);
+            yield return new WaitForSeconds(speedFactor * 0.000000001f);
             if(++wordIndex == text.Length)
             {
                 state = State.COMPLETED;
